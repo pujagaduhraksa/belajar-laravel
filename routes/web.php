bf086c614c54b368a052a93e75c5826ab4b9f9b0
pujagaduhraksa <pujaraksa.id@gmail.com>
2021-04-02 09:40:11 +0800

    belajar Route

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,38 @@
 |
 */
 
+// Routing Default
 Route::get('/', function () {
-    // return view('welcome');
-    return view('index');
+    return view('welcome');
 });
+
+Route::get('/mahasiswa', function(){
+    echo 'Mahasiswa';
+});
+
+Route::get('/ubg/ilkom/mahasiswa', function(){
+    return 'Mahasiswa Ilkom Kampus UBG';
+});
+
+// Routing Dengan Parameter
+Route::get('/ubg/ilkom/mahasiswa/{semester}/{nim}', function($semester, $nim){
+    return 'Mahasiswa Ilkom Kampus UBG, SEMESTER : ' . $semester . ' NIM : ' . $nim;
+});
+
+// Route Redirect
+Route::redirect('/login', '/masuk');
+
+// Route Group
+Route::group(['prefix' => '/admin'], function(){
+
+    Route::get('/mahasiswa', function(){
+        return 'Admin Mahasiswa';
+    });
+    Route::get('/dosen', function(){
+        return 'Admin Dosen';
+    });
+    Route::get('/pegawai', function(){
+        return 'Admin Pegawai';
+    });
+});
+
